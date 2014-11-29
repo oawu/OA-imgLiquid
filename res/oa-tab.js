51664eff364499ddae11f1ddd3fb40b15c1e2332
OA Wu <comdan66@gmail.com>
2014-11-30 00:28:34 +0800

    Add oa tab.

--- res/oa-tab.js
+++ res/oa-tab.js
@@ -2,20 +2,53 @@
  * @author      OA Wu <comdan66@gmail.com>
  * @copyright   Copyright (c) 2014 OA Wu Design
  */
+
+(function( factory ) {
+    if ((typeof define === 'function') && define.amd) define (['jquery'], factory);
+    else factory (jQuery);
+}(function ($) {
+
+  $.fn.extend ({
+    OAtab: function (opt) {
+      var d4Opt = {
+        titleSelector: '.title',
+        panelSelector: '.panel',
+      }
+
+      var init = function (opt) {
+        var $title = $(this).find (opt.titleSelector).addClass ('oa-tab-title').clone (true, true);
+        var $panel = $(this).find (opt.panelSelector).addClass ('oa-tab-panel').clone (true, true);
+        $(this).addClass ('oa-tab').empty ();
+
+        $('<div />').addClass ('oa-tab-titles').append (
+            $('<div />').addClass ('oa-tab-titles-container').addClass ('clearfix').append ($title)
+          ).appendTo ($(this));
+
+        $('<div />').addClass ('oa-tab-shadow-line').appendTo ($(this));
+
+        $('<div />').addClass ('oa-tab-panels').append (
+            $('<div />').addClass ('oa-tab-panels-container').addClass ('clearfix').append ($panel)
+          ).appendTo ($(this));
+
+        $title.on ('mouseenter click', function () {
+            var n = $(this).parent ().children ().removeClass ('active').index ($(this).addClass ('active')),
+            $rn = $panel.eq (n);
+            $panel.parent ().css ({'height': parseFloat ($rn.css ('height')) + parseFloat ($rn.css ('margin-top')) + parseFloat ($rn.css ('margin-bottom')) + 'px'});
+
+            $panel.map (function (i) {
+              $(this).css ({'left': (i - n) * (parseFloat ($rn.css ('width')) + parseFloat ($rn.css ('margin-left')) + parseFloat ($rn.css ('margin-right'))) + 'px'});
+            })
+          }).eq (0).click ()
+      }
+
+      opt = $.extend({}, d4Opt, opt);
+
+      init.bind ($(this), opt).apply ();
+    }
+  });
+}));
 $(function () {
 
-  var $firstRank = $('.ranks .ranks_container .rank').eq (0);
-  var rankWidth = parseFloat ($firstRank.css ('width'));
-  var rankMarginLeft = parseFloat ($firstRank.css ('margin-left'));
-  var rankMarginRight = parseFloat ($firstRank.css ('margin-right'));
-  $('.items .items_container .item').on ('mouseenter click',function () {
-    var n = $(this).parent ().children ().removeClass ('active').index ($(this).addClass ('active'));
-    var $r = $('.ranks .ranks_container .rank').eq (n);
-    var nh = parseFloat ($r.css ('height')) + parseFloat ($r.css ('margin-top')) + parseFloat ($r.css ('margin-bottom'));
-    $('.ranks .ranks_container').css ({'height': nh + 'px'});
+  $('.tab').OAtab ();
 
-    $('.ranks .ranks_container .rank').map (function (i) {
-      $(this).css ({'left': (i - n) * (rankWidth + rankMarginLeft + rankMarginRight) + 'px'});
-    })
-  }).filter ('.active').click ();
 });
